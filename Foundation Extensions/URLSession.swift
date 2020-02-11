88a0813acbe5d16784b8676b010d7e5b4fdceeaa
Ruben Santiago <ruben.santiago@aa.com>
2020-02-11 16:29:17 -0600

    Add table view controller

--- Foundation Extensions/URLSession.swift
+++ Foundation Extensions/URLSession.swift
@@ -11,10 +11,12 @@ extension URLSession {
     fileprivate func codableTask<T: Codable>(with url: URL,
                                              completionHandler: @escaping (T?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
         return self.dataTask(with: url) { data, response, error in
+            
             guard let data = data, error == nil else {
                 completionHandler(nil, response, error)
                 return
             }
+            
             completionHandler(try? self.newJSONDecoder().decode(T.self, from: data), response, nil)
         }
     }
